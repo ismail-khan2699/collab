e99eeac17a3e96b95b52a272d9fe6d3fde34a24c
ismail-khan2699 <animeking4434@gmail.com>
2024-06-07 11:11:16 +0500

    component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Navbar from "./Components/Navbar";
 import HeroSection from "./Components/HeroSection";
+import Component from "./Components/component";
 
 export default function Home() {
   return (
     <>
 
-     <Navbar/>
-     <HeroSection/>
+      <Navbar />
+      <HeroSection />
+      <Component />
 
     </>
   );
