08861dc29e6ed898a31927b6967d6c9b914bc12c
Mudassir-Saleem <mudassirsaleem5555@gmail.in>
2024-06-08 15:57:16 +0500

    Your commit message

---
+++ app/Components/Newsection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Newsection = () => {
+  return (
+    <div className='py-16 bg-pink-500'>  
+      <h3 className='  text-center mt-8 justify-center text-2xl font-bold'>Robx.Ai</h3>
+    </div>
+  )
+}
+
+export default Newsection
